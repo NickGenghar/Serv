51b450d08c184c94a3d7252f54e4387eede82d59
Nick Genghar <www.genghar15@gmail.com>
2020-04-05 10:46:47 +0000

    Fixed a few querks

--- bot.js
+++ bot.js
@@ -33,24 +33,24 @@ fs.access('./data', (e) => {
     }
 });
 
-fs.access('./temp', (e) => {
-    if(e) {
-        fs.mkdir('./temp', (e) => {
-            if(e) throw e;
-        });
-    }
-});
-
 //Clear temp folder at startup
 let clear = () => {
-    fs.readdir('./temp', (e, f) => {
-        if(e) throw e;
-        let files = f.filter(e => {if(e.indexOf('.') > -1) return e});
-        files.forEach(g => {
-            fs.unlink(`./temp/${g}`, e => {
-                if(e) return console.log(e);
+    fs.access('./temp', (e) => {
+        if(e) {
+            fs.mkdir('./temp', (e) => {
+                if(e) throw e;
             });
-        });
+        } else {
+            fs.readdir('./temp', (e, f) => {
+                if(e) throw e;
+                let files = f.filter(e => {if(e.indexOf('.') > -1) return e});
+                files.forEach(g => {
+                    fs.unlink(`./temp/${g}`, e => {
+                        if(e) return console.log(e);
+                    });
+                });
+            });
+        }
     });
 }
 
