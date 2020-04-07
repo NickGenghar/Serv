54212cac7eb8e2b95b196889319ec6ead2efe2d3
Nick Genghar <www.genghar15@gmail.com>
2020-04-07 18:12:44 +0800

    Fixed issue not be able to use literal emoji

--- commands/Administrator/reaction.js
+++ commands/Administrator/reaction.js
@@ -7,7 +7,7 @@ O = new Object;
 
 O.name = 'reaction';
 O.alias = ['reaction', 'react', 'r', 'hand'];
-O.desc = 'Create a message with reaction which when reacted, given a role to the user.';
+O.desc = 'Create a message with reaction which when reacted, give a role to the user.';
 O.usage = [
     '//reaction <Channel> <Role> <Emoji> <Message>',
     'Channel: The channel for the reaction message.',
@@ -21,11 +21,11 @@ O.run = async (msg, args, queue) => {
     const svr = JSON.parse(fs.readFileSync(`./data/guilds/${msg.guild.id}.json`));
     if(svr.modRole.length <= 0) return msg.channel.send('No Moderator Role Set.');
     if(!msg.guild.member(msg.author).roles.cache.find(r => svr.modRole.includes(r.id))) return msg.channel.send('You do not have the required moderation role.');
-    if(!svr.modules.includes(O.name)) return msg.channel.send('This module is not activated. Please activate it via the `setup` command.');
+    if(!svr.modules.includes('reaction')) return msg.channel.send('This module is not activated. Please activate it via the `setup` command.');
 
     let verifyChannel = msg.mentions.channels.first() || msg.guild.channels.cache.get(args[0]);
     let verifyRole = msg.mentions.roles.first() || msg.guild.roles.cache.get(args[1]);
-    let reactionEmoji = msg.guild.emojis.cache.get(args[2]);
+    let reactionEmoji = msg.client.emojis.cache.find(i => i.toString() == args[2]) || msg.client.emojis.cache.get(args[2]);
 
     if(!verifyChannel) return msg.channel.send('Verification channel unspecified.');
     if(!verifyRole) return msg.channel.send('Verification role unspecified.');
