6c4cedfc381dc9259d57736953449b09244e6fab
unknown <WinTG@WINTG.WINTG>
2020-06-25 14:13:55 +0800

    Fixed issue in raw event based reaction role.

--- bot.js
+++ bot.js
@@ -58,12 +58,12 @@ bot.on('raw', tag => {
     if(tag.t == 'MESSAGE_REACTION_ADD') {
         let svr = JSON.parse(fs.readFileSync(`./data/guilds/${tag.d.guild_id}.json`));
         if(svr.reactions.length > 0) {
-            let payload = svr.reactions.find(v => {return v.msg == tag.d.message_id && v.emoji == tag.d.emoji_id});
+            let payload = svr.reactions.find(v => {return v.msg == tag.d.message_id && v.emoji == tag.d.emoji.id});
             if(payload) {
                 let guild = bot.guilds.cache.find(i => i.id == tag.d.guild_id);
                 let role = guild.roles.cache.find(i => i.id == payload.role);
                 let member = guild.members.cache.find(i => i.id == tag.d.user_id);
-                if(member.user.bot) {
+                if(!member.user.bot) {
                     member.roles.add(role)
                     .catch(e => {
                         if(e) throw e;
@@ -74,12 +74,12 @@ bot.on('raw', tag => {
     } else if(tag.t == 'MESSAGE_REACTION_REMOVE') {
         let svr = JSON.parse(fs.readFileSync(`./data/guilds/${tag.d.guild_id}.json`));
         if(svr.reactions.length > 0) {
-            let payload = svr.reactions.find(v => {return v.msg == tag.d.message_id && v.emoji == tag.d.emoji_id});
+            let payload = svr.reactions.find(v => {return v.msg == tag.d.message_id && v.emoji == tag.d.emoji.id});
             if(payload) {
                 let guild = bot.guilds.cache.find(i => i.id == tag.d.guild_id);
                 let role = guild.roles.cache.find(i => i.id == payload.role);
                 let member = guild.members.cache.find(i => i.id == tag.d.user_id);
-                if(member.user.bot) {
+                if(!member.user.bot) {
                     member.roles.remove(role)
                     .catch(e => {
                         if(e) throw e;
